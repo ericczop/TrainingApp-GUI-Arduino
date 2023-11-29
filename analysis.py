14c71e10508913573b6ba1891df12e1a4e0319dd
ericczop <eczop@o2.pl>
2023-11-29 17:29:09 +0100

    Analysis file updated, convolving data, finding peaks

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.signal import find_peaks
 
 
 class DataAnalysis:
@@ -7,18 +8,17 @@ class DataAnalysis:
         self.data = data
         self.distance = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
         self.kilograms = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
+        self.peaks = None
+        self.mins = None
+
+    def smooth_data(self, window_size=300):
+        smoothed_data = np.convolve(self.data, np.ones(window_size) / window_size, mode='valid')
+        return smoothed_data
 
     def count_reps(self):
-        reps = 0
-        switch = False
-
-        for i in range(len(self.data) - 1):
-            if self.data[i] + 2 < self.data[i + 1] and not switch:
-                reps += 1
-                switch = True
-            if self.data[i] > self.data[i + 1] and switch:
-                switch = False
-        return reps
+        self.peaks = find_peaks(self.smooth_data(), prominence=2, distance=500)
+        print(self.peaks[0])
+        return len(self.peaks[0])
 
     def count_kilograms(self):
         actual_height = round(self.data[0], 1)
@@ -32,14 +32,20 @@ class DataAnalysis:
         return actual_kilograms
 
     def plot_data(self):
-        x_points = np.arange(start=0, stop=len(self.data), step=1)
-        y_points = np.array(self.data)
+        x_points = np.arange(start=0, stop=len(self.smooth_data()), step=1)
+        y_points = self.smooth_data()
 
         plt.plot(x_points, y_points)
-        plt.xlabel("Czas (s)")
-        plt.ylabel("Odległość (cm)")
+        plt.xlabel("Samples quantity")
+        plt.ylabel("Distance (cm)")
 
-        plt.text(x_points[-1], y_points[-1], f"Kilograms assumed: {self.count_kilograms()} kg", fontsize=10, ha='right')
+        plt.text(x_points[-1], y_points[-1], f"Kilograms: {self.count_kilograms()} kg", fontsize=10, ha='right')
 
+        if self.peaks is not None:
+            [plt.axvline(p, c='red', linewidth=1) for p in self.peaks[0]]
+        if self.mins is not None:
+            [plt.axvline(p, c='blue', linewidth=1) for p in self.mins[0]]
+
+        plt.axvline(x_points[0], c='black', linewidth=1)
+        plt.axvline(x_points[-1], c='black', linewidth=1)
         plt.show()
-
